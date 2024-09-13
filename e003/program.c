ca002b0bd08f98473c77311f02d8996be12fec77
sergio-ramon <sergioramon.sr98@hotmail.com>
2024-09-13 09:16:05 -0300

    Atualização do código do ex003
    Implementação de novas funções para confirmação, limpeza de tela por os e reinício ou finalização, além de refatoração de alguns pontos.

--- e003/program.c
+++ e003/program.c
@@ -5,8 +5,7 @@
 // execução do programa
 void infoInicial(){
     printf(
-        "\a"
-        "\nCALCULADORA DE JUROS COMPOSTOS\n"
+        "CALCULADORA DE JUROS COMPOSTOS\n"
         "-----------------------------------"
         "\nBem vindo a calculadora de Juros compostos!\n"
         "\nOs dados inseridos devem ser separados por \".\" no lugar\n"
@@ -15,9 +14,65 @@ void infoInicial(){
         "-----------------------------------\n\n");
 }
 
+// Limpa as informações que estão no Buffer
 void limpaBuffer(){
     int c;
     while ((c = getchar()) != '\n' && c != EOF){};
+}
+
+// Limpa a tela de acordo com o sistema operacional
+void limpaTela(){
+    #ifdef _WIN32
+        system("cls"); // Caso esteja no Windows
+    #else
+        system("clear"); // Caso esteja no Linux ou Mac
+    #endif
+}
+
+// Coleta os dados digitados e armazena nas variáveis de main
+float coletaDados(char *msg, float lmtInf, float lmtSup){
+    float valor;
+    int result;
+
+    do
+    {
+        printf("%s", msg);
+        result = scanf("%f", &valor);
+        limpaBuffer();
+    } while (result != 1 || valor < lmtInf || valor > lmtSup);
+
+    return valor; 
+}
+
+// Verifica se o usuário deseja prosseguir ou alterar os dados
+int confirma(float apIni, float apMen, float t, float tAp, int temp){
+    int decisao, resultC;
+
+    // Limpa a tela
+    limpaTela();
+
+    //Exibe os dados informados
+    printf(
+        "Confira os dados informados:\n"
+        "Aporte inicial\t\t\t:\tR$ %.2f\n"
+        "Aporte mensal\t\t\t:\tR$ %.2f\n"
+        "Taxa de retorno anual\t\t:\t%.2f%%\n"
+        "Taxa de correção de aporte\t:\t%.2f%%\n"
+        "Tempo em anos\t\t\t:\t%d\n", apIni, apMen, t, tAp, temp);
+
+    do{
+        printf(
+        "\n------------------------------\n"
+        "Deseja alterar os dados ou prosseguir?\n"
+        "1 - Alterar dados informados\n"
+        "2 - Prosseguir para o cálculo\n"
+        "------------------------------\n"
+        "Escolha a opção desejada: ");
+        resultC = scanf("%d", &decisao);
+        limpaBuffer();
+    }while (resultC != 1 || decisao <= 0 || decisao >= 3);
+
+    return decisao;
 }
 
 // Pega as informações e printa na tela, mês a mês até a quantidade
@@ -27,9 +82,11 @@ void geraMeses(int m, float apIni, float apMen, float j, float t){
         "Mês nº %d\n"
         "-----------------------------\n",
         m);
+    // Caso seja o primeiro mês, consideramos o aporte inicial
     if (m < 2){
         printf("Aporte\t:\tR$ %.2f\n", apIni);
     }
+    // A partir do segundo mês, consideramos o aporte mensal
     else{
         printf("Aporte\t:\tR$ %.2f\n", apMen);
     }
@@ -40,103 +97,112 @@ void geraMeses(int m, float apIni, float apMen, float j, float t){
         j, t);
 }
 
-void calculoMensal(int x, float *j, float *t, float taxa, float ap){
+// Calculamos os juros mensais e atualizamos o valor total
+void calculoMensal(float *j, float *t, float taxa, float ap){
     *j = *t * ((taxa/100)/12);
     *t += ap + *j;
 }
 
+// Atualizamos os totais que serão exibidos no final do programa
 void totais(float *apTotal, float apMen, float *jTotal, float j){
     *apTotal += apMen;
     *jTotal += j;
 }
 
+// Opções de término de programa
+int fim(){
+    int opcao;
+
+    do {
+        printf(
+            "1 - Novo cálculo\t\t2 - Finalizar\n"
+            "-----------------------------------------------\n"
+            "Escolha a opção desejada: ");
+        scanf("%d", &opcao);
+        limpaBuffer();
+    }while (opcao < 1 || opcao > 2);
+
+    return opcao == 1 ? 1 : 0; // Retorna 1 (novo cálculo), 0 (fim)
+}
+
+// Início do programa
 int main(){
-    float aporteInicial = 0, aporteMensal = 0, taxa = 0, taxaAporte = 0;
-    float total = 0, juros = 0, aporteTotal = 0, jurosTotais = 0;
-    int tempo = 0, count = 0, result;
-    
-    // Chama as infos da tela incial
-    infoInicial();
+    do {
+        float aporteInicial = 0, aporteMensal = 0, taxa = 0, taxaAporte = 0;
+        float total = 0, juros = 0, aporteTotal = 0, jurosTotais = 0;
+        int tempo = 0, count = 0, result;
+        
+        do{
+            // Limpa a tela
+            limpaTela();
 
-    // Coleta os dados do aporte, taxa e tempo do usuário
-    do{
-        printf("Digite o aporte inicial: ");
-        result = scanf("%f", &aporteInicial);
-        limpaBuffer();
-    }while (result != 1 || aporteInicial < 0);
-    do{
-        printf("Digite o aporte mensal: ");
-        result = scanf("%f", &aporteMensal);
-        limpaBuffer();
-    }while (result != 1 || aporteMensal < 0);
-    do{
-        printf("Digite a taxa anual: ");
-        result = scanf("%f", &taxa);
-        limpaBuffer();
-    }while (result != 1 || taxa < 0 || taxa > 100);
-    do{
-        printf("Digite a taxa de correção de aporte: ");
-        result = scanf("%f", &taxaAporte);
-        limpaBuffer();
-    }while (result != 1 || taxaAporte < 0 || taxaAporte > 100);
-    do{
-        printf("Digite o tempo em anos: ");
-        result = scanf("%i", &tempo);
-        limpaBuffer();
-    }while (result != 1 || tempo <= 0);
+            // Chama as infos da tela incial
+            infoInicial();
 
-    // Atualiza o total final com o valor de aporte inicial
-    aporteTotal += aporteInicial;
-    
-    // Fazemos um for loop para que os cálculos sejam executados
-    // até a quantidade de meses desejada pelo usuário.
-    for (int i = 0; i <= tempo*12; i++) {
-        // Caso seja o início da contagem, mostramos como 1º ano
-        if (i == 0) {
-            printf(
-                "\n------------------------------------ "
-                "%dº ano\n", ((i/12)+1));
-        }
-        // Se não, seguimos contando os anos posteriores
-        else {
-            // Caso seja o primeiro mês, levaremos em conta o aporte
-            // inicial para começo de cálculos
-            if (i < 2){
-                calculoMensal(i, &juros, &total, taxa, aporteInicial);
-            }
-            else {
-                calculoMensal(i, &juros, &total, taxa, aporteMensal);
-            }
-            
-            // A cada 12 meses imprimimos na tela o número do ano que
-            // será calculado a partir do próximo mês
-            if (count == 12){
+            // Coleta os dados do aporte, taxa e tempo do usuário
+            aporteInicial = coletaDados("Digite o aporte inicial: ", 0, 1000000000);
+            aporteMensal = coletaDados("Digite o aporte mensal: ", 0, 1000000000);
+            taxa = coletaDados("Digite a taxa anual: ", 0, 100);
+            taxaAporte = coletaDados("Digite a taxa de correção de aporte: ", 0, 100);
+            tempo = (int) coletaDados("Digite o tempo em anos: ", 1, 1000);
+        }while (confirma(aporteInicial, aporteMensal, taxa, taxaAporte, tempo) == 1); 
+
+        // Atualiza o total final com o valor de aporte inicial
+        aporteTotal += aporteInicial;
+
+        // Limpa a tela
+        limpaTela();
+        
+        // Fazemos um for loop para que os cálculos sejam executados
+        // até a quantidade de meses desejada pelo usuário.
+        for (int i = 0; i <= tempo*12; i++) {
+            // Caso seja o início da contagem, mostramos como 1º ano
+            if (i == 0) {
                 printf(
                     "------------------------------------ "
                     "%dº ano\n", ((i/12)+1));
-
-                // Usamos a informação de taxa de correção para corrigir
-                // o valor do aporte a cada ano
-                aporteMensal += aporteMensal * (taxaAporte/100);
-                count = 0;
             }
-            
-            geraMeses(i, aporteInicial, aporteMensal, juros, total);
+            // Se não, seguimos contando os anos posteriores
+            else {
+                // Caso seja o primeiro mês, levaremos em conta o aporte
+                // inicial para começo de cálculos
+                if (i < 2){
+                    calculoMensal(&juros, &total, taxa, aporteInicial);
+                }
+                else {
+                    calculoMensal(&juros, &total, taxa, aporteMensal);
+                }
+                
+                // A cada 12 meses, imprimimos na tela o número do ano que
+                // será calculado a partir do próximo mês
+                count++;
+                if (count == 12){
+                    printf(
+                        "------------------------------------ "
+                        "%dº ano\n", ((i/12)+1));
 
-            // Atualização das variáveis para apresentação no final
-            totais(&aporteTotal, aporteMensal, &jurosTotais, juros);
-            count++;
+                    // Usamos a informação de taxa de correção para corrigir
+                    // o valor do aporte a cada ano
+                    aporteMensal += aporteMensal * (taxaAporte/100);
+                    count = 0;
+                }
+                
+                geraMeses(i, aporteInicial, aporteMensal, juros, total);
+
+                // Atualização das variáveis para apresentação no final
+                totais(&aporteTotal, aporteMensal, &jurosTotais, juros);
+            }
         }
-    }
 
-    // Retornamos os dados totais ao usuário
-    printf(
-        "Seus resultados após %d anos, considerando %.2f%% a.a:\n"
-        "-------------------------------------------------------\n"
-        "Aporte total\t:\tR$ %.2f\n"
-        "Juros totais\t:\tR$ %.2f\n"
-        "Total acumulado\t:\tR$ %.2f\n\n",
-        tempo, taxa, aporteTotal, jurosTotais, total);
+        // Retornamos os dados totais ao usuário
+        printf(
+            "Seus resultados após %d anos, considerando %.2f%% a.a:\n"
+            "------------------------------------------------------------\n"
+            "Aporte total realizado\t\t\t:\tR$ %.2f\n"
+            "Juros totais ganhos\t\t\t:\tR$ %.2f\n"
+            "Total acumulado ao final do período\t:\tR$ %.2f\n\n",
+            tempo, taxa, aporteTotal, jurosTotais, total);
+    }while (fim() == 1);
 
     return 0;
 }
