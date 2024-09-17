aacb94a98adf575c5b0950d855683ab003bcaeaf
sergio-ramon <sergioramon.sr98@hotmail.com>
2024-09-17 10:22:02 -0300

    Atualização ex001
    Refatoração do código fonte e adição de comentários

--- e001/program.c
+++ e001/program.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+#include <locale.h>
 
+// Coleta o número de referência e armazena na variável (numero) em main
+void coletaDado(char msg[], int *num){
+    printf("%s", msg);
+    scanf("%i", num);
+}
+
+// Início do programa
 int main (){
-    int numero;
+    system("chcp 65001");
+    setlocale(LC_ALL, "Português");
 
-    printf("Digite um número: ");
-    scanf("%i", &numero);
+    int numero, quadrado;
+    float raiz;
 
-    float raiz = sqrt(numero);
-    int quadrado = pow(numero, 2);
-    printf("A raiz quadrada dele é: %.2f\n", raiz);
-    printf("Quando elevado ao quadrado, temos: %d\n", quadrado);
+    coletaDado("Digite um número: ", &numero);
+
+    raiz = sqrt(numero);
+    quadrado = pow(numero, 2);
+
+    printf(
+        "\nA raiz quadrada dele é: %.2f\n"
+        "Quando elevado ao quadrado, temos: %d\n", raiz, quadrado);
 
     return (0);
 }
