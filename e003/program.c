3e7846d041273cf3c218d48752fd3020d95b4300
sergio-ramon <sergioramon.sr98@hotmail.com>
2024-09-17 09:38:06 -0300

    Adição de comentários

--- e003/program.c
+++ e003/program.c
@@ -17,7 +17,10 @@ void infoInicial(){
 
 // Limpa as informações que estão no Buffer
 void limpaBuffer(){
-    int c;
+    int c; // Variável para armazenar valor do Buffer
+
+    // Armazena cada valor do Buffer por vez
+    // Verifica se cada valor do Buffer é igual a \n
     while ((c = getchar()) != '\n' && c != EOF){};
 }
 
@@ -35,8 +38,7 @@ float coletaDados(char *msg, float lmtInf, float lmtSup){
     float valor;
     int result;
 
-    do
-    {
+    do{
         printf("%s", msg);
         result = scanf("%f", &valor);
         limpaBuffer();
@@ -49,8 +51,7 @@ float coletaDados(char *msg, float lmtInf, float lmtSup){
 int confirma(float apIni, float apMen, float t, float tAp, int temp){
     int decisao, resultC;
 
-    // Limpa a tela
-    limpaTela();
+    limpaTela(); // Limpa a tela
 
     //Exibe os dados informados
     printf(
@@ -70,7 +71,7 @@ int confirma(float apIni, float apMen, float t, float tAp, int temp){
         "------------------------------\n"
         "Escolha a opção desejada: ");
         resultC = scanf("%d", &decisao);
-        limpaBuffer();
+        limpaBuffer(); // Limpeza do Buffer
     }while (resultC != 1 || decisao <= 0 || decisao >= 3);
 
     return decisao;
@@ -113,8 +114,8 @@ void totais(float *apTotal, float ap, float *jTotal, float j){
 // Opções de término de programa
 int fim(){
     int opcao;
-
-    do {
+    
+    do { // Looping de verificação de resposta do usuário
         printf(
             "\nO que deseja fazer?\n\n"
             "1 - Novo cálculo\t\t2 - Finalizar\n"
@@ -122,6 +123,7 @@ int fim(){
             "Escolha a opção desejada: ");
         scanf("%d", &opcao);
         limpaBuffer();
+    // Repetimos até que a resposta seja 1 ou 2
     }while (opcao < 1 || opcao > 2);
 
     return opcao == 1 ? 1 : 0; // Retorna 1 (novo cálculo), 0 (fim)
@@ -129,20 +131,20 @@ int fim(){
 
 // Início do programa
 int main(){
+    // Seta a linguagem para corrigir problemas de acentuação no terminal
     system("chcp 65001");
     setlocale(LC_ALL, "Português");
 
-    do {
+    do { // Looping do programa até finalização
         float aporteInicial = 0, aporteMensal = 0, taxa = 0, taxaAporte = 0;
         float total = 0, juros = 0, aporteTotal = 0, jurosTotais = 0;
         int tempo = 0, count = 0, result;
         
-        do{
-            // Limpa a tela
-            limpaTela();
+        do{ // Looping de coleta de dados
 
-            // Chama as infos da tela incial
-            infoInicial();
+            limpaTela(); // Limpa a tela
+            
+            infoInicial(); // Chama as infos da tela incial
 
             // Coleta os dados do aporte, taxa e tempo do usuário
             aporteInicial = coletaDados("Digite o aporte inicial: ", 0, 1000000000);
@@ -152,9 +154,9 @@ int main(){
             tempo = (int) coletaDados("Digite o tempo em anos: ", 1, 1000);
         }while (confirma(aporteInicial, aporteMensal, taxa, taxaAporte, tempo) == 1);
 
-        // Limpa a tela
-        limpaTela();
+        limpaTela(); // Limpa a tela
 
+        // Imprime a primeira linha de marcação de ano no início
         printf(
             "------------------------------------ "
             "%dº ano\n", 1);
@@ -162,17 +164,18 @@ int main(){
         // Fazemos um for loop para que os cálculos sejam executados
         // até a quantidade de meses desejada pelo usuário.
         for (int i = 1; i <= tempo*12; i++) {
-            // A cada 12 meses, imprimimos na tela o número do ano que
-                // será calculado a partir do próximo mês
-                if (count == 12){
-                    printf(
-                        "------------------------------------ "
-                        "%dº ano\n", ((i/12)+1));
 
-                    // Usamos a informação de taxa de correção para corrigir o valor do aporte a cada ano
-                    aporteMensal += aporteMensal * (taxaAporte/100);
-                    count = 0;
-                }
+            // A cada 12 meses, imprimimos na tela o número do ano que
+            // será calculado a partir do próximo mês
+            if (count == 12){
+                printf(
+                    "------------------------------------ "
+                    "%dº ano\n", ((i/12)+1));
+
+                // Usamos a informação de taxa de correção para corrigir o valor do aporte a cada ano
+                aporteMensal += aporteMensal * (taxaAporte/100);
+                count = 0;
+            }
             // Se não, seguimos contando os anos posteriores
             else {
                 // Caso seja o primeiro mês, levaremos em conta o aporte
@@ -189,7 +192,6 @@ int main(){
                 }
 
                 geraMeses(i, aporteInicial, aporteMensal, juros, total);
-                
                 count++;
             }
         }
